e19ac57915b8de065f8f431ad923210069662b69
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-27 21:08:48 +0200

    CPS wasn't as hard as we tought

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,17 +1,33 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.launch
 import java.util.*
 
 class Model {
-    fun count(): Int = Random().nextInt(10)
+    fun count(f: (Int) -> Unit) {
+        async {
+            f(Random().nextInt(10))
+        }
+    }
 }
 
 fun main(args: Array<String>) {
     val model = Model()
 
-    if (model.count() == 0) {
-        println("We don't have data")
-    } else {
-        println("We have ${model.count()} items")
+    model.count { count ->
+        if (count == 0) {
+            println("We don't have data")
+        } else {
+            println("We have $count items")
+        }
+    }
+
+    Thread.sleep(1000)
+}
+
+fun async(completion: () -> Unit) {
+    launch(CommonPool) {
+        completion()
     }
 }
