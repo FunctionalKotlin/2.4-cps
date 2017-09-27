b7dc5ff5bba326881d413dd6f81f2d56666d0105
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-27 21:03:07 +0200

    The access is synchronous, repeat the access is synchronous

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -5,3 +5,13 @@ import java.util.*
 class Model {
     fun count(): Int = Random().nextInt(10)
 }
+
+fun main(args: Array<String>) {
+    val model = Model()
+
+    if (model.count() == 0) {
+        println("We don't have data")
+    } else {
+        println("We have ${model.count()} items")
+    }
+}
